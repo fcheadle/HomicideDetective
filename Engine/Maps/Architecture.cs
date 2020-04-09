99e6a56109841e6de8db2ab6014d5e40aa29119c
Forrest Cheadle <fcheadle@gmail.com>
2020-04-09 07:58:36 -0600

    Revert "deleted many files, stashing to restore a deleted folder"
    This reverts commit 597a0e4c108702b192039013f204d865c18abd42.

---
+++ Engine/Maps/Architecture.cs
@@ -0,0 +1,12 @@
+﻿namespace Engine.Maps
+{
+    internal class Architecture
+    {
+        private object house;
+
+        public Architecture(object house)
+        {
+            this.house = house;
+        }
+    }
+}
