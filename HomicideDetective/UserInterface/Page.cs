81b389515432ba6960d38fc60fd6ad0040053ef7
Forrest Cheadle <fcheadle@gmail.com>
2021-05-29 19:21:22 -0600

    added Several Write methods to Page, added methods for upline/downline, back a page, and next page. added some test key commands.

--- HomicideDetective/UserInterface/Page.cs
+++ HomicideDetective/UserInterface/Page.cs
@@ -1,10 +1,7 @@
 ﻿using System.Collections.Generic;
-using GoRogue.GameFramework.Components;
-using HomicideDetective.Mysteries;
+using HomicideDetective.Happenings;
 using SadConsole;
 using SadConsole.Components;
-using SadConsole.UI;
-using SadConsole.UI.Controls;
 using SadRogue.Integration.Components;
 using SadRogue.Primitives;
 
@@ -18,60 +15,23 @@ namespace HomicideDetective.UserInterface
         //SadConsole Controls
         public ScreenSurface TextSurface { get; private set; }
         public ScreenSurface BackgroundSurface { get; private set; }
-        public Button PreviousButton { get; private set; }
-        public Button NextButton { get; private set; }
-        public ControlHost ButtonBar { get; private set; }
+        public int Width { get; }
+        public int Height { get; }
         
         //the contents printed by the cursor
-        public IGameObjectComponent Component { get; }
-        public string Contents { get; private set; }
-        public List<string> PastContents { get; private set; }
+        public List<string> Contents { get; private set; }= new List<string>();
+        public int PageNumber { get; private set; } = 0;
         
-        public Page() : base(true, false, true, true)
+        public Page(int width, int height) : base(true, false, true, true)
         {
-            InitContents();
+            Width = width;
+            Height = height;
             InitWindow();
-            //InitButtons();//todo
-        }
-
-        private void InitButtons()
-        {
-            ButtonBar = new ControlHost();
-            
-            PreviousButton = new Button(4,3)
-            {
-                MouseArea = new Rectangle((0, 0), (4, 3)),
-                CanFocus = false,
-            };
-            PreviousButton.Surface.DefaultBackground = Color.Tan;
-            PreviousButton.Surface.DefaultForeground = Color.Blue;
-            PreviousButton.Text = "<-";
-            ButtonBar.Add(PreviousButton);
-
-            NextButton = new Button(4,3)
-            {
-                MouseArea = new Rectangle((BackgroundSurface.Surface.Width - 4, 0), (BackgroundSurface.Surface.Width, 3)),
-                CanFocus = false,
-            };
-            PreviousButton.Surface.DefaultBackground = Color.Tan;
-            PreviousButton.Surface.DefaultForeground = Color.Blue;
-            PreviousButton.Text = "->";
-            
-            ButtonBar.Add(PreviousButton);
-            
-            ButtonBar.Add(NextButton);
-            
-            BackgroundSurface.SadComponents.Add(ButtonBar);
-        }
-
-        private void InitContents()
-        {
-            PastContents = new List<string>();
         }
 
         private void InitWindow()
         {
-            BackgroundSurface = new ScreenSurface(Program.Width / 3 + 1, Program.Height)
+            BackgroundSurface = new ScreenSurface(Width, Height)
             {
                 IsVisible = true,
                 IsFocused = false,
@@ -79,12 +39,12 @@ namespace HomicideDetective.UserInterface
             };
             BackgroundSurface.Surface.Fill(Color.Blue, Color.Tan, '_');
             
-            TextSurface = new ScreenSurface(Program.Width / 3 - 1, Program.Height)
+            TextSurface = new ScreenSurface(Width - 3, Height * 3)
             {
                 IsVisible = true,
                 IsFocused = false,
                 FocusOnMouseClick = true,
-                Position = (1,0)
+                Position = (1,1)
             };
             
             var cursor = new Cursor()
@@ -96,17 +56,100 @@ namespace HomicideDetective.UserInterface
             BackgroundSurface.Children.Add(TextSurface);
         }
         
-        public void Print(string contents)
+        private void PrintContents(int index)
+        {
+            var cursor = TextSurface.GetSadComponent<Cursor>();
+            Clear();
+            cursor.Position = (0, 0);
+            cursor.Print(new ColoredString($"{Contents[index]}", Color.Blue, Color.Transparent));
+        }
+
+        private void PrintContents() => PrintContents(PageNumber);
+        
+        public void Write(string contents)
+        {
+            Clear();
+            if(!Contents.Contains(contents))
+                Contents.Add(contents);
+
+            PageNumber = Contents.Count - 1;
+            PrintContents();
+        }
+
+        public void Write(IEnumerable<string> contents)
+        {
+            Clear();
+            var content = "";
+            foreach (var thing in contents)
+                content += $"{thing}\r\n";
+            
+            Write(content);
+        }
+
+        public void Write(Happening happening)
+        {
+            List<string> contents = new();
+            contents.Add(happening.When.ToString());
+            contents.Add($"Location: ${happening.Where}");
+
+            //var things = "Things involved: ";
+            // foreach(var thing in happening.What)
+            var people = "People Involved: ";
+            foreach (var person in happening.Who)
+                people += $"{person}, ";
+            contents.Add($"Occurence: ${happening.What}");
+        }
+
+        public void Write(Substantive substantive)
+        {
+            List<string> contents = new();
+            contents.Add(substantive.Name!);
+            contents.Add(substantive.Description!);
+            contents.Add(substantive.GenerateDetailedDescription());
+            contents.AddRange(substantive.Details);
+            Write(contents);
+        }
+
+        public static void BackPage()
+        {
+            var page = Program.Page;
+            if (page.PageNumber > 0)
+                page.PrintContents(--page.PageNumber);
+        }
+        public static void ForwardPage()
+        {
+            var page = Program.Page;
+            if (page.PageNumber < page.Contents.Count - 1)
+                page.PrintContents(++page.PageNumber);
+        }
+
+        public static void UpOneLine()
+        {
+            var page = Program.Page;
+            page.TextSurface.Surface.ShiftDown();
+        }
+
+        public static void DownOneLine()
+        {
+            var page = Program.Page;
+            page.TextSurface.Surface.ShiftUp();
+        }
+
+        //temporary - for testing
+        public static void WriteGarbage()
+        {
+            var page = Program.Page;
+            page.Clear();
+            string contents = "home ";
+            for (int i = 0; i < 1000; i++)
+                contents += $"{(char)(i % 256)} ";
+            
+            page.Write(contents);
+        }
+
+        private void Clear()
         {
             TextSurface.Surface.Clear();
-            if(!PastContents.Contains(Contents))
-                PastContents.Add(Contents);
-
-            Contents = contents;
-            var cursor = TextSurface.GetSadComponent<Cursor>();
-            cursor.Position = (0, 0);
-
-            cursor.Print(new ColoredString($"{Contents}", Color.Blue, Color.Transparent));
         }
     }
 }
